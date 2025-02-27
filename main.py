3cb86a57b4a602a4a83433e4b14d6b31d7bf94ae
sickdawg5440 <kjkuhner@gmail.com>
2025-02-27 04:17:13 -0800

    working word count and char count

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words
+from stats import count_words, get_chars_dict
 
 def get_book_text():
     with open("books/frankenstein.txt") as words:
@@ -8,6 +8,7 @@ def get_book_text():
 def main():
     book_text = get_book_text()
     num_words = count_words(book_text)
+    chars_dict = get_chars_dict(book_text)
     print(f"{num_words} words found in the document")
-
+    print(chars_dict)
 main()
