e3449036180e7f35921d081a5da356e85369ca11
sickdawg5440 <kjkuhner@gmail.com>
2025-02-27 04:59:32 -0800

    Completed

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
-from stats import count_words, get_chars_dict
+from stats import count_words, get_chars_dict, chars_dict_sorted
+import sys
 
-def get_book_text():
-    with open("books/frankenstein.txt") as words:
+def main():
+    if len(sys.argv) != 2:  
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1) 
+    book_path = sys.argv[1]
+    book_text = get_book_text(book_path)
+    num_words = count_words(book_text)
+    chars_dict = get_chars_dict(book_text)
+    chars_sorted = chars_dict_sorted(chars_dict)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in chars_sorted:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
+    print("============= END ===============")
+    
+def get_book_text(book_path):
+    with open(book_path) as words:
         book = words.read()
     return book
 
-def main():
-    book_text = get_book_text()
-    num_words = count_words(book_text)
-    chars_dict = get_chars_dict(book_text)
-    print(f"{num_words} words found in the document")
-    print(chars_dict)
+
+   
+
 main()
--- stats.py
+++ stats.py
@@ -11,3 +11,13 @@ def get_chars_dict(text):
         else:
             chars[lowered] = 1
     return chars
+
+def sort_on(d):
+    return d["num"]
+
+def chars_dict_sorted(num_chars_dict):
+    sorted_list = []
+    for char in num_chars_dict:
+        sorted_list.append({"char": char, "num": num_chars_dict[char]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
